8b10dfa60ebe8cf9c9517a9df35d14da66e12fa7
hadar-goldstein <hadargol71@gmail.com>
2025-01-03 15:28:39 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+// window.onload = test();
+// function test(){
+//     const testDiv = document.getElementById("testDiv");
+//     let content = "";
+//     for(let i = 1; i <=100; i++) {
+//        content +=
+//        `${i} <br>`;
+//     }
+//     testDiv.innerHTML = content;
+// }
