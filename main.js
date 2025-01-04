35528b20c441f61b0aa8354d6deb78b7498135ab
hadar-goldstein <hadargol71@gmail.com>
2025-01-04 23:59:11 +0200

    modal handling added

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
 /// <reference path="jquery-3.7.1.js"/>
 "use strict";
 
-// Only for tests
-// **************
+// Get DOM Elements
+const capacityModal = document.getElementById("capacityModal");
+const closeModal = document.getElementById("closeModal");
+
+// On Load 
+// *******
 window.onload = function () {
-    // test();
     getCoinsData();
 };
-// function test() {
-//     const testDiv = document.getElementById("testDiv");
-//     let content = "";
-//     for (let i = 1; i <= 100; i++) {
-//         content +=
-//             `${i} <br>`;
-//     }
-//     testDiv.innerHTML = content;
-// }
+
 
 // Parallax Animation 
 // ******************
@@ -48,21 +43,87 @@ function displayCoins(coins) {
     for (let i = 0; i <= coins.length; i++) {
         let coin = coins[i];
         content += `
-        <div class="card">
+        <div class="card" id="${coin.id}">
             <div class="coinIdentity">
                 <span class="symbolSpan">${coin.symbol}</span>
                 <span class="symbolName">${coin.name}</span>
                 <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                    <input onclick="selectedCoins('${coin.id}')" id="${coin.id}-toggle" class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
                 </div>
             </div>
 
         <span>
-            <button class="infoBtn">More Information</button>
+            <button onclick="getMoreInfo('${coin.id}')" class="infoBtn">More Information</button>
         </span>
         </div>`;
         if (i === 20)
             break;
     }
     cardsContainer.innerHTML += content;
+}
+
+
+async function getMoreInfo(id) {
+    const url = `https://api.coingecko.com/api/v3/coins/${id}`;
+    const response = await axios.get(url);
+    const coin = response.data;
+    displayMoreInfoCard(coin, id);
+}
+
+function displayMoreInfoCard(coin, id) {
+    const coinDiv = document.getElementById(`${id}`);
+    coinDiv.innerHTML = `
+    <span>${coin.market_data.current_price.eur} EUR</span>
+    <span>${coin.market_data.current_price.usd} USD</span>
+    <span>${coin.market_data.current_price.ils} ILS</span>
+    `;
+}
+
+let coinsArray = [];
+let modal = document.getElementById("myModal");
+function selectedCoins(id){
+    const checkbox = document.getElementById(`${id}-toggle`);
+    const isChecked = checkbox.checked;
+    console.log(isChecked); 
+
+    if(isChecked) {
+        const coin = id;
+        coinsArray.push(coin);
+        console.log(coinsArray); 
+    }
+    else {
+        for(let i = 0; i < coinsArray.length; i++){
+            if(coinsArray[i] === id)
+                coinsArray.splice(i, 1);
+            }
+        console.log(coinsArray); 
+
+    }
+
+    if(coinsArray.length === 6){
+        modalHandling()
+    }
+}
+
+
+
+function modalHandling() {
+    const selectedCoinsList = document.getElementById("selectedCoinsList");
+    capacityModal.style.display = "flex";
+    let content = "";
+        for(const item of coinsArray){
+            content+= `
+            <div>
+                <input type="radio">
+                <label>${item}</label><br>
+            </div>`;
+        }
+
+    selectedCoinsList.innerHTML = content;
+
+    if(closeModal){
+        closeModal.addEventListener("click", () => {
+            capacityModal.style.display = "none";
+        });
+    }
 }
