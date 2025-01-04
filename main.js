cb1dce9880ad8278a0392a073fee0f427d5c55e3
hadar-goldstein <hadargol71@gmail.com>
2025-01-04 17:35:04 +0200

    Cards CSS and JS added

--- main.js
+++ main.js
@@ -4,17 +4,18 @@
 // Only for tests
 // **************
 window.onload = function () {
-    test();
+    // test();
+    getCoinsData();
 };
-function test() {
-    const testDiv = document.getElementById("testDiv");
-    let content = "";
-    for (let i = 1; i <= 100; i++) {
-        content +=
-            `${i} <br>`;
-    }
-    testDiv.innerHTML = content;
-}
+// function test() {
+//     const testDiv = document.getElementById("testDiv");
+//     let content = "";
+//     for (let i = 1; i <= 100; i++) {
+//         content +=
+//             `${i} <br>`;
+//     }
+//     testDiv.innerHTML = content;
+// }
 
 // Parallax Animation 
 // ******************
@@ -31,3 +32,37 @@ $(window).scroll(() => {
     else
         $("nav a").css("color", "#d9d9d9");
 });
+
+// --------------------------------------------------------------------------------------------------
+
+async function getCoinsData() {
+    const url = "https://api.coingecko.com/api/v3/coins/list";
+    const response = await axios.get(url);
+    const coins = response.data;
+    displayCoins(coins);
+}
+
+function displayCoins(coins) {
+    const cardsContainer = document.getElementById("cardsContainer");
+    let content = "";
+    for (let i = 0; i <= coins.length; i++) {
+        let coin = coins[i];
+        content += `
+        <div class="card">
+            <div class="coinIdentity">
+                <span class="symbolSpan">${coin.symbol}</span>
+                <span class="symbolName">${coin.name}</span>
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                </div>
+            </div>
+
+        <span>
+            <button class="infoBtn">More Information</button>
+        </span>
+        </div>`;
+        if (i === 20)
+            break;
+    }
+    cardsContainer.innerHTML += content;
+}
