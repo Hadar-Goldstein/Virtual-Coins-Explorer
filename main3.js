3640a2ec60b431ab9843536a5e88cf63b11fb80d
hadar-goldstein <hadargol71@gmail.com>
2025-01-26 17:50:39 +0200

    IIFE added to main3.js

--- main3.js
+++ main3.js
@@ -1,19 +1,20 @@
 /// <reference path="jquery-3.7.1.js"/>
 "use strict";
 
+(()=>{
 
-// Parallax Animation 
-// ******************
-$(window).scroll(() => {
-    const scrollPositionY = $(window).scrollTop();
-    const speed = 0.4;
-    $(".parallaxImage").css("transform", `translate3d(0, ${scrollPositionY * speed}px, 0)`);
-
-    const backgroundHeight = $(".parallaxBg").outerHeight();
-    if (backgroundHeight <= scrollPositionY)
-        $("nav a").css("color", "black");
-    else
+    // Parallax Animation 
+    // ******************
+    $(window).scroll(() => {
+        const scrollPositionY = $(window).scrollTop();
+        const speed = 0.4;
+        $(".parallaxImage").css("transform", `translate3d(0, ${scrollPositionY * speed}px, 0)`);
+        
+        const backgroundHeight = $(".parallaxBg").outerHeight();
+        if (backgroundHeight <= scrollPositionY)
+            $("nav a").css("color", "black");
+        else
         $("nav a").css("color", "#d9d9d9");
-});
-
-// --------------------------------------------------------------------------------------------------
+    });
+    
+})();
