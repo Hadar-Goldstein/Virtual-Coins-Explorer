7196b827846e61d96a42829ad07092a6e8aff7eb
hadar-goldstein <hadargol71@gmail.com>
2025-02-02 10:20:51 +0200

    IIFE added to mainLiveReport.js

--- mainLiveReports.js
+++ mainLiveReports.js
@@ -1,157 +1,157 @@
 "use strict";
 
+(() => {
+    // Parallax Animation 
+    // ******************
+    $(window).scroll(() => {
+        const scrollPositionY = $(window).scrollTop();
+        const speed = 0.4;
 
-// Parallax Animation 
-// ******************
-$(window).scroll(() => {
-    const scrollPositionY = $(window).scrollTop();
-    const speed = 0.4;
+        $(".parallaxImage").css("transform", `translate3d(0, ${scrollPositionY * speed}px, 0)`);
 
-    $(".parallaxImage").css("transform", `translate3d(0, ${scrollPositionY * speed}px, 0)`);
-
-    const backgroundHeight = $(".parallaxBg").outerHeight();
-    if (backgroundHeight <= scrollPositionY)
-        $("nav a").css("color", "black");
-    else
-        $("nav a").css("color", "#d9d9d9");
-});
-
-// --------------------------------------------------------------------------------------------------
-
-// Get Data from local storage
-const symbolsString = JSON.parse(localStorage.getItem('symbolsString'));
-let errorLogic = false;
-
-if(symbolsString === null) errorLogic = true;
-else if(symbolsString.length === 0) errorLogic = true;
-
-if(errorLogic) {
-    alert("Live Report can't be displayed because no coins were selected");
-    $(document).ready(function () {
-        let previousPage = document.referrer;
-
-        if (previousPage) 
-            window.location.href = previousPage;
-        else 
-            window.location.href = "index.html";
+        const backgroundHeight = $(".parallaxBg").outerHeight();
+        if (backgroundHeight <= scrollPositionY)
+            $("nav a").css("color", "black");
+        else
+            $("nav a").css("color", "#d9d9d9");
     });
-}
 
-// Create string for chart's subtitle
-const splitted = symbolsString.split(",");
-const subtitle = splitted.join(", ");
+    // --------------------------------------------------------------------------------------------------
 
-// Create Static Data for chart
-let coinDataPoints = {};
+    // Get Data from local storage
+    const symbolsString = JSON.parse(localStorage.getItem('symbolsString'));
+    let errorLogic = false;
 
-function getDataArray() {
-    const dataArray = [];
-    const coins = subtitle.split(", ");
+    if (symbolsString === null) errorLogic = true;
+    else if (symbolsString.length === 0) errorLogic = true;
 
-    for (const coin of coins) {
-        coinDataPoints[coin] = [];
+    if (errorLogic) {
+        alert("Live Report can't be displayed because no coins were selected");
+        $(document).ready(function () {
+            let previousPage = document.referrer;
 
-        const obj = {
-            type: "spline",
-            name: coin,
-            showInLegend: true,
-            xValueFormatString: "hh:mm:ss TT",
-            yValueFormatString: "$#,##0.#",
-            dataPoints: coinDataPoints[coin]
-        };
-
-        dataArray.push(obj);
+            if (previousPage)
+                window.location.href = previousPage;
+            else
+                window.location.href = "index.html";
+        });
     }
 
-    return dataArray;
-}
+    // Create string for chart's subtitle
+    const splitted = symbolsString.split(",");
+    const subtitle = splitted.join(", ");
 
+    // Create Static Data for chart
+    let coinDataPoints = {};
 
-function getDataFromApi() {
-    setInterval(async function () {
-        try {
-            const apiKey = "de74120914e79001714c5592aac92671b8bacbf2f50377763b1e657b292277d8";
-            const url = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${symbolsString}&tsyms=USD&api_key=${apiKey}`;
-            const response = await axios.get(url);
-            const pricesObj = response.data;
+    function getDataArray() {
+        const dataArray = [];
+        const coins = subtitle.split(", ");
 
-            updateChartData(pricesObj);
+        for (const coin of coins) {
+            coinDataPoints[coin] = [];
+
+            const obj = {
+                type: "spline",
+                name: coin,
+                showInLegend: true,
+                xValueFormatString: "hh:mm:ss TT",
+                yValueFormatString: "$#,##0.#",
+                dataPoints: coinDataPoints[coin]
+            };
+
+            dataArray.push(obj);
         }
-        catch(err) {
-            console.log("Can't get data from API");
-            alert(err.message);
-        }
-    }, 2000);
-}
 
-// Update Chart 
-function updateChartData(pricesObj) {
-    for (const coin in pricesObj) {
-        if (coinDataPoints[coin]) {
-            const yValue = pricesObj[coin].USD;
-            const currentTime = new Date();
+        return dataArray;
+    }
 
-            coinDataPoints[coin].push({
-                x: currentTime,
-                y: yValue
-            });
+    function getDataFromApi() {
+        setInterval(async function () {
+            try {
+                const apiKey = "de74120914e79001714c5592aac92671b8bacbf2f50377763b1e657b292277d8";
+                const url = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${symbolsString}&tsyms=USD&api_key=${apiKey}`;
+                const response = await axios.get(url);
+                const pricesObj = response.data;
 
-            // Handling up to 100 points 
-            if (coinDataPoints[coin].length > 100) {
-                coinDataPoints[coin].shift();
+                updateChartData(pricesObj);
+            }
+            catch (err) {
+                console.log("Can't get data from API");
+                alert(err.message);
+            }
+        }, 2000);
+    }
+
+    // Update Chart 
+    function updateChartData(pricesObj) {
+        for (const coin in pricesObj) {
+            if (coinDataPoints[coin]) {
+                const yValue = pricesObj[coin].USD;
+                const currentTime = new Date();
+
+                coinDataPoints[coin].push({
+                    x: currentTime,
+                    y: yValue
+                });
+
+                // Handling up to 100 points 
+                if (coinDataPoints[coin].length > 100) {
+                    coinDataPoints[coin].shift();
+                }
             }
         }
+
+        chart.render();
     }
 
-    chart.render();
-}
+    // JQuery`s function 
+    let chart;
 
-// JQuery`s function 
-let chart;
+    $(window).on("load", function () {
+        const dataArray = getDataArray();
 
-window.onload = function () {
-    const dataArray = getDataArray();
+        const options = {
+            animationEnabled: true,
+            title: {
+                text: "Coins Live Reports"
+            },
+            subtitles: [{
+                text: subtitle
+            }],
+            axisX: {
+                title: "Time"
+            },
+            axisY: {
+                title: "Values",
+                titleFontColor: "#4F81BC",
+                lineColor: "#4F81BC",
+                labelFontColor: "#4F81BC",
+                tickColor: "#4F81BC",
+            },
+            toolTip: {
+                shared: true
+            },
+            legend: {
+                cursor: "pointer",
+                itemclick: toggleDataSeries
+            },
+            data: dataArray
+        };
 
-    const options = {
-        animationEnabled: true,
-        title: {
-            text: "Coins Live Reports"
-        },
-        subtitles: [{
-            text: subtitle
-        }],
-        axisX: {
-            title: "Time"
-        },
-        axisY: {
-            title: "Values",
-            titleFontColor: "#4F81BC",
-            lineColor: "#4F81BC",
-            labelFontColor: "#4F81BC",
-            tickColor: "#4F81BC",
-        },
-        toolTip: {
-            shared: true
-        },
-        legend: {
-            cursor: "pointer",
-            itemclick: toggleDataSeries
-        },
-        data: dataArray
-    };
+        // Chart creation
+        chart = $("#chartContainer").CanvasJSChart(options).CanvasJSChart();
 
-    // Chart creation
-    chart = $("#chartContainer").CanvasJSChart(options).CanvasJSChart();
-
-    function toggleDataSeries(e) {
-        if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-            e.dataSeries.visible = false;
-        } else {
-            e.dataSeries.visible = true;
+        function toggleDataSeries(e) {
+            if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                e.dataSeries.visible = false;
+            } else {
+                e.dataSeries.visible = true;
+            }
+            e.chart.render();
         }
-        e.chart.render();
-    }
 
-    // Set data 
-    getDataFromApi();
-}
+        // Set data 
+        getDataFromApi();
+    });
+})();
